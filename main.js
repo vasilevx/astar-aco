15a058d8d1ce6714523c1c3271ab10c91c0445f9
Nick <barmatun101@mail.ru>
2019-06-01 03:11:20 +0300

    A*

--- main.js
+++ main.js
@@ -1,88 +1,104 @@
 const data = {
-    1: [2, 3],
-    2: [1, 3, 4, 5] ,
-    3: [1, 2, 4, 7, 8],
-    4: [2, 3, 6, 7],
-    5: [2, 6],
-    6: [4, 5, 9],
-    7: [3, 4, 9, 10],
-    8: [3, 8],
-    9: [5, 6, 7, 10],
-    10: [7, 8, 9, 10]
-
-
+    "A": ["B", "C", "A"],
+    "B": ["A", "C", "D", "E"] ,
+    "C": ["A", "B", "D", "G", "H"],
+    "D": ["B", "C", "F", "G"],
+    "E": ["B", "F"],
+    "F": ["D", "E", "I"],
+    "G": ["C", "D", "I", "J"],
+    "H": ["C"],
+    "I": ["E", "F", "G", "J"],
+    "J": ["G", "H", "I"]
 }
 
-// nodes: [
-//     { data: { id: 'a' } },
-//     { data: { id: 'b' } },
-//     { data: { id: 'c' } },
-//     { data: { id: 'd' } },
-//     { data: { id: 'e' } }
-//   ],
+let start, finish;
 
-//   edges: [
-//     { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
-//     { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
-//     { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
-//     { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
-//     { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
-//     { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
-//     { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
-//   ]
-function graphFromData(data) {
-    const nodes = [], edges = [];
-    Object.keys(data).map((key, i) => {
-        nodes.push({data: {id: String(key)}});
-        data[key].forEach((v, j) => {
-            if (!edges.find(edge => {
-                // console.dir(edge);
-                return false;
-                return (edge.data.source == v && edge.data.target == key) || (edge.data.source == key && edge.data.target == v)
+document.addEventListener('input', function(e) {
+  const element = e.target;
+  if (element.type != 'text') return;
+  if (element == startInput) {
+    cy.$(`#${start}`).removeClass('start');
+    start = element.value.toUpperCase();
+    if (cy.$(`#${start}`).length == 1)
+    cy.$(`#${start}`).addClass('start');
+  }
+  if (element == finishInput) {
+    cy.$(`#${finish}`).removeClass('finish');
+    finish = element.value.toUpperCase();
+    if (cy.$(`#${finish}`).length == 1)
+      cy.$(`#${finish}`).addClass('finish');
+  }
+});
 
-            })) {
-                edges.push({data: {id: "" + i + j, weight: Math.floor(Math.random() * (1000 - 1)) + 1, source: String(key), target: String(v)}})
+    function graphFromData(data) {
+      const nodes = [], edges = [];
+      Object.keys(data).map((key) => {
+          nodes.push({
+            data: {
+              id: String(key)
             }
-        });
-    });
-
-    return {nodes, edges};
-    
-
-}
-
-let g = graphFromData(data);
-
-var cy = cytoscape({
-    container: document.getElementById('container'),
+          });
+          data[key].forEach((v) => {
+              if (!edges.find(edge => {
+                  return (edge.data.source == v && edge.data.target == key) || (edge.data.source == key && edge.data.target == v)
   
-    // boxSelectionEnabled: false,
-    // autounselectify: true,
-  
-    style: cytoscape.stylesheet()
-      .selector('node')
-        .style({
-          'label': 'data(id)',
-          'background-color': '#f00',
-          'text-valign': 'center',
-          'text-halign': 'center'
-        })
-      .selector('edge')
-        .style({
-          'curve-style': 'bezier',
-          'target-arrow-shape': 'triangle-backcurve',
-          'width': 4,
-          'content': 'data(weight)',
-          'line-color': '#ccc',
-          'target-arrow-color': '#000',
-          'arrow-scale': 1.5
-        })
-    .selector(':selected')
-    .style({
-        'background-color': '#0f0',
-        'line-color': '#00f',
-     
-    })
+              })) {
+                  edges.push({data: {id: `${key}${v}`, weight: Math.floor(Math.random() * (20 - 1)) + 1, source: String(key), target: String(v)}})
+              }
+          });
+      });  
+      return {nodes, edges};
+  }  
+  let graph = graphFromData(data);  
+  let a = cytoscape({
+    elements: {
+      nodes: graph.nodes,    
+      edges: graph.edges
+    }
+  });
+
+
+  let cy = cytoscape({
+      container: document.getElementById('container'),   
+      style: cytoscape.stylesheet()
+        .selector('node')
+          .style({
+            'label': 'data(id)',
+            'background-color': '#acc',
+            'text-valign': 'center',
+            'text-halign': 'center',
+            'width': '70',
+            'height': '70'
+          })
+          .selector('.aStar')
+          .style({
+            'label': 'data(lbl)'
+          })
+        .selector('edge')
+          .style({
+            'curve-style': 'bezier',
+            // 'target-arrow-shape': 'triangle-backcurve',
+            'width': 6,
+            'content': 'data(weight)',
+            'color': '#000',
+            'font-size': 18,
+            'line-color': '#ccc',
+          })
+      .selector(':selected')
+      .style({
+          'background-color': '#0f0',
+          'line-color': '#00f',  
+          'transition-property': 'background-color, line-color',
+          'transition-duration': '0.5s'     
+      })
+      .selector('.start')
+      .style({
+        'background-color': 'pink',
+      })
+      .selector('.finish')
+      .style({
+        'background-color': 'red',
+      })
       .selector('.highlighted')
         .style({
           'background-color': '#0f0',
@@ -90,40 +106,126 @@ var cy = cytoscape({
           'transition-property': 'background-color, line-color, target-arrow-color',
           'transition-duration': '0.5s'
         }),
-  
-    elements: {
-        nodes: g.nodes,
-  
-        edges: g.edges
-      },
-  
-    layout: {
-      name: 'breadthfirst',
-      directed: false,
-      roots: '#1',
-      padding: 10
+    
+      elements: {
+          nodes: graph.nodes,    
+          edges: graph.edges
+        },
+    
+      layout: {
+        name: 'breadthfirst',
+        directed: false,
+        roots: '#A',
+        padding: 10
+      }
+    });
+
+    function unselect() {
+      cy.elements().unselect();
     }
-  });
-  
 
-  var d = cy.elements().dijkstra({root:'#1', weight: function(e) {
-      return e.data('weight');
-  }, directed: true});
-  var dd = d.pathTo( cy.$('#10') );
-    // dd.select();
 
-  var astar = cy.elements().aStar({root: '#1', goal: '#10', heuristic: function(v) {console.log(v._private.data.id);return v.data('id')}, weight:function(edge) {return edge.data('weight')}, directed: true});
-  astar.path.select();
-  var i = 0;
-  var highlightNextEle = function(){
 
-     if( i < astar.path.length ){
-         astar.path[i].addClass('highlighted');
 
+
+    function aStar() {
+      if (!(start && finish)) return;
+      unselect();
+      const fw = cy.elements().floydWarshall({
+        weight: edge => edge.data('weight')
+      });
+      const nodeIDs = [];
+      graph.nodes.forEach(node => {
+        node.data.h = fw.distance(`#${finish}`, `#${node.data.id}`);
+        node.data.lbl = `${node.data.id} (${node.data.h})`;
+        nodeIDs.push(node.data.id);
+
+      });
+      
+
+
+
+      cy.elements('node').addClass('aStar');
+      const startNode = cy.elements(`#${start}`);
+      const finishNode = cy.elements(`#${finish}`);
+
+      const g = graph.nodes.reduce((gv, v) => {
+        gv[v.data.id] = fw.distance(startNode, `#${v.data.id}`);
+        return gv;
+      }, {});
+
+      const f = {};
+
+      const sortByHeuristic = (node1, node2) => f[node1.data('id')] > f[node2.data('id')] ? -1 : 1;
+
+      let currNode;
+      let prevID;
+
+      const queue = [];
+      const visited = new Set();
+      queue.push(startNode);
+
+      while (queue.length > 0) {
+        console.log('Очередь: ', queue.map(el => el.data('id')).join(', '));
+        console.log('Вершины не в очереди: ', nodeIDs.filter(id => !queue.map(el => el.data('id')).includes(id)).join(', '));
+        currNode = queue.pop();
+        currNode.edgesWith(`#${prevID}`).select();
+        visited.add(currNode);
+        currNode.select();
+        console.log('Посещенные вершины: ', Array.from(visited).map(el => el.data('id')).join(', '));
+
+
+
+        if (currNode.data('id') == finishNode.data('id')) {
+          return true;
+        }
+        const neighbors = currNode.neighborhood().nodes().map(node => node);
+
+        let tempG = g[currNode.data('id')];
+        for (let node of neighbors) {
+          let d = currNode.edgesWith(node).data('weight');
+          let tScore = d + tempG;
+
+
+          if (visited.has(node) && tScore >= g[node.data('id')]) continue;
+
+          if (!visited.has(node) || tScore < g[node.data('id')]) {
+            g[node.data('id')] = tScore;
+            f[node.data('id')] = g[node.data('id')] + node.data('h');
+          }
+          
+          if(!queue.includes(node))
+            queue.push(node);
+            queue.sort(sortByHeuristic); 
+
+        } 
+        prevID = currNode.data('id');
         
-      i++;
-      setTimeout(highlightNextEle, 3);
-     }
-  };
+        console.log('-----');
+      }
+      return false;   
+    }
 
-//   highlightNextEle();
+    function ACO(graph) {
+
+      let V=[];			//Vertex Set
+      let algoOn=0;
+      let a=[[0],[0]]//Adjacency Matrix
+      let n=0;			//Nodes
+      let i,j;
+      //let iteration
+      let A=[];			///Ants
+      let destination=3;
+      let p=[[0],[0]];
+      let antNumber=4;
+      let p_max=150;		//Max Pheromone
+      let e_factor;	//Evaporation Factor
+      let iACO=0;			//Counter
+      let best=-1;
+      let alphaC=1,beta=0;
+      let gount=5;
+    }
+
+
+
+
